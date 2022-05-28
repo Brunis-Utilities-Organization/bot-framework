78ba00cc4d231284940b1e06b047612abcfd1ff3
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2022-05-27 22:35:22 -0700

    Revert "progress"
    This reverts commit 425dc31859a945be7e3905cfbf13ae04803e8ffe.

--- lib/bot_framework/command_loader.ts
+++ lib/bot_framework/command_loader.ts
@@ -1,8 +1,7 @@
 import { readdir } from "node:fs/promises";
-import { SlashCommand, PreprocessorOptions } from "./command_classes.js"
-import { Logger } from "./logger.js"
+import { SlashCommand } from "./command_classes.js"
 
-const logger = new Logger("CommandLoader")
+const logger = new Logger
 
 export async function searchForCommands(dir: string): Promise<string[]> {
     const files = await readdir(dir);
@@ -18,23 +17,7 @@ export async function loadCommands(cmds: string[]): Promise<SlashCommand[]> {
         try {
             slashCommand = await import(`./${cmd}`);
         } catch (err) {
-          logger.error(`Failed to load command file! Error: \n${err.stack}`)
-        }
 
-        if (slashCommand.slashCommand) {
-          if (slashCommand !instanceof slashCommand) {
-            logger.debug(`Command ${cmd} is not a valid command file, skipping...`)
-          } else {
-            let existingSlashCommand = undefined
-            for (const cmd in ret) { if (ret.name.toLowerCase() == cmd.slashCommand.slashCommand.name.toLowerCase()) existingSlashCommand = true; }
-            
-            if (existingSlashCommand) {
-              throw new Error(`Failed to load slash command "${cmd.slashCommand.slashCommand.name}": There already exists a command under that name.`)
-            } else {
-              ret.push(cmd.slashCommand)
-              logger.debug(`Loaded command ${cmd.slashCommand.slashCommand.name}.`)
-            }
-          }
         }
     }
 }
